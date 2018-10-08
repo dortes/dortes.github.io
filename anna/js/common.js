c3ea7e8cbf1b33489575532501ad63d976ddb302
dortezyy <dortezy@gmail.com>
2018-10-09 03:47:17 +0500

    yeah

--- anna/js/common.js
+++ anna/js/common.js
@@ -2,10 +2,14 @@ $(function() {
 
 	$('.mobile-bar').on('click', function(){
 	$('.overlay').css("width", '100%');
+	$('.title').hide();
+	$('.background').hide();
 })
 
 	$('.close-button').on('click', function(){
 		$('.overlay').css("width", '0');
+		$('.title').show();
+		$('.background').show();
 	})
 
 
