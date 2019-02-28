cb23928fb2836c5b2e0fd42abf5a3ae3f1a670e5
dortezyy <dortezy@gmail.com>
2019-02-28 10:26:53 +0100

    some

--- website/js/common.js
+++ website/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
 
-	// Custom JS
+	$('.main-menu .mob-button').on('click', function() {
+		
+		if($('.main-menu .menu').hasClass('active')) {
+			$('.main-menu .menu').removeClass('active');
+		}
+		else {
+			$('.main-menu .menu').addClass('active');
+		}
+	})
+
+	$('.main-menu .close').on('click', function() {
+		$('.main-menu .menu').removeClass('active');
+	})
 
 });
