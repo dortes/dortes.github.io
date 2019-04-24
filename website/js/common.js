1a609bc07a1156d2d9b382982a840de451ac9c0f
Dortes <dortezy@gmail.com>
2019-04-24 16:03:03 +0200

    as

--- website/js/common.js
+++ website/js/common.js
@@ -1,17 +1,5 @@
 $(function() {
 
-	$('.main-menu .mob-button').on('click', function() {
-		
-		if($('.main-menu .menu').hasClass('active')) {
-			$('.main-menu .menu').removeClass('active');
-		}
-		else {
-			$('.main-menu .menu').addClass('active');
-		}
-	})
-
-	$('.main-menu .close').on('click', function() {
-		$('.main-menu .menu').removeClass('active');
-	})
+	// Custom JS
 
 });
